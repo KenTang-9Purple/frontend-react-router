3b6a2e66a9f7ac5ec233dfdf5edc6221e0f2a1f9
KenTang-9Purple <tangtszyik@gmail.com>
2024-12-03 13:00:37 +0800

    20241203 3rd

--- src/components/PageLink.jsx
+++ src/components/PageLink.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 function PageLink({link,itemClass}) {
   return (link.href.includes("https://") || link.href.includes("http://")  ?
-    <li><Link to={link.href} className={itemClass}>{link.text}</Link></li>:
-    <li><a href={link.href} className={itemClass}>{link.text}</a></li>
+    <li><a href={link.href} className={itemClass}>{link.text}</a></li>:
+    <li><Link to={link.href} className={itemClass}>{link.text}</Link></li>
   )
 }
 
